2d5d9a6f2a0ad70d4e56d9d484b5b0f50015df76
Xruptor <202515+Xruptor@users.noreply.github.com>
2022-11-02 09:44:17 -0400

    Updated for Dragonflight retail changes. Updated TOC. Added additional support for Classic, WOTLK, etc...

--- XanBuffTimers.lua
+++ XanBuffTimers.lua
@@ -518,7 +518,7 @@ function addon:CreateBuffTimers()
     Frm.spellText2:SetJustifyH("RIGHT")
     Frm.spellText2:SetPoint("RIGHT", Frm.icon, "LEFT" , 20, 0)
 	
-	Frm.Bar = Frm:CreateFontString(nil, "GameFontNormal")
+	Frm.Bar = Frm:CreateFontString(nil, "OVERLAY")
 	Frm.Bar:SetFont(STANDARD_TEXT_FONT, 14, "OUTLINE, MONOCHROME")
 	Frm.Bar:SetText(BAR_TEXT)
 	Frm.Bar:SetPoint("LEFT", Frm.icon, "RIGHT", 33, 0)
